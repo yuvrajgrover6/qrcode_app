f46e497d81242903a8173bff91c26952523ae646
avnotaklu <sukhmander111101@gmail.com>
2022-08-06 15:37:20 +0530

    caption saving and history view changes

--- lib/modules/history/view/history_view.dart
+++ lib/modules/history/view/history_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:qrcode_app/constants.dart';
 import 'package:qrcode_app/modules/history/controller/history_controller.dart';
 import 'package:qrcode_app/modules/qr_scanner/model/qr_snapshot.dart';
 
@@ -10,47 +11,63 @@ class HistoryView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Get.create(() => HistoryController(), permanent: false);
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     return GetBuilder<HistoryController>(builder: (controller) {
-      return FutureBuilder<Map<dynamic, QRSnapshot>>(
-          future: controller.qrSnapshotHistory,
-          builder:
-              ((context, AsyncSnapshot<Map<dynamic, QRSnapshot>> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            if (snapshot.data != null) {
-              if (snapshot.data!.isNotEmpty) {
-                return ListView.builder(
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: ((context, index) {
-                      return ListTile(
-                        title:
-                            Text(snapshot.data!.values.toList()[index].value),
-                        subtitle: Text(DateFormat("dd-MM-yy HH::mm::ss").format(
-                            snapshot.data!.values.toList()[index].time)),
-                        trailing: IconButton(
-                            onPressed: () => controller.deleteQRSnapshot(
-                                snapshot.data!.keys.toList()[index]),
-                            icon: Icon(Icons.delete)),
-                      );
-                    }));
-              } else {
-                return const Center(
-                    child: Text(
-                  "There is no previous activity",
-                  style: TextStyle(fontSize: 20),
-                ));
-              }
-            } else {
-              return const Center(
-                  child: Text(
-                "There was problem fetching your history",
-                style: TextStyle(fontSize: 20),
-              ));
-            }
-          }));
+      return Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: controller.deleteAllQRSnapshot,
+            child: Icon(Icons.delete_sweep),
+            backgroundColor: kPrimaryColor,
+          ),
+          body: FutureBuilder<Map<dynamic, QRSnapshot>>(
+              future: controller.qrSnapshotHistory,
+              builder:
+                  ((context, AsyncSnapshot<Map<dynamic, QRSnapshot>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (snapshot.data != null) {
+                  if (snapshot.data!.isNotEmpty) {
+                    return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: ((context, index) {
+                          final qrSnapshot =
+                              snapshot.data!.values.toList()[index];
+                          return ExpansionTile(
+                            title: Text(qrSnapshot.value),
+                            subtitle: Text(DateFormat("dd-MM-yy HH::mm::ss")
+                                .format(qrSnapshot.time)),
+                            trailing: IconButton(
+                                onPressed: () => controller.deleteQRSnapshot(
+                                    snapshot.data!.keys.toList()[index]),
+                                icon: Icon(Icons.delete)),
+                            children: [
+                              ListTile(
+                                title: Text(qrSnapshot.caption),
+                                horizontalTitleGap: 0,
+                              )
+                            ],
+                          );
+                        }));
+                  } else {
+                    return const Center(
+                        child: Text(
+                      "There is no previous activity",
+                      style: TextStyle(fontSize: 20),
+                    ));
+                  }
+                } else {
+                  return const Center(
+                      child: Text(
+                    "There was problem fetching your history",
+                    style: TextStyle(fontSize: 20),
+                  ));
+                }
+              })));
     });
   }
 }
